77acb62d51c87f718bf3c64ebe59eb820bf5b51b
Meryem <76478333+mebenyahia@users.noreply.github.com>
2022-06-28 08:10:44 +0100

    Update 1981 bruteforce.py

--- 1981. Minimize the Difference Between Target and Chosen Elements/1981 bruteforce.py
+++ 1981. Minimize the Difference Between Target and Chosen Elements/1981 bruteforce.py
@@ -1,3 +1,10 @@
+'''
+Brute force solution
+Exceeds Memory Limit
+
+Algo: Generate all possible product combinations for list mat, then compute the absolute difference between the target and the sum of each. 
+Then return the minimum.
+'''
 class Solution:
     def minimizeTheDifference(self, mat: List[List[int]], target: int) -> int:
         import itertools
