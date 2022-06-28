edde26c5ded4808386a233837d23d39fcbbfd797
Meryem <76478333+mebenyahia@users.noreply.github.com>
2022-06-28 21:07:29 +0100

    Add files via upload

---
+++ 1100. Find K-Length Substrings With No Repeated Characters/1100 my solution.py
@@ -0,0 +1,20 @@
+'''
+Used Sliding Window Algorithmic mental model
+Simply have a deque, rotate and if all elements are unique in window: increment n
+There are probably faster ways to do it, but I'm happy I was able to do it as a newbie :D
+Runs in linear time
+
+'''
+class Solution:
+    def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
+        from collections import deque
+        
+        if k>len(s):
+            return 0
+        deq = deque(list(s))
+        n=0
+        for i in range(len(s)-k+1):
+            if len(set(list(deq)[:k])) == len(list(deq)[:k]):
+                n+= 1
+            deq.rotate(-1)
+        return n
